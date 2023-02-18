29669b2a859ca61cc6af9b42406ea08109474aa7
dennis githinji <wakahiad@gmail.com>
2023-02-18 16:16:54 +0300

    config server setup

---
+++ config-server/src/main/java/com/densoft/configServer/ConfigServer.java
@@ -0,0 +1,16 @@
+package com.densoft.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+}
